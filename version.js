163be3ab4e3e25d4ec2b08ea5e8d80860b28db0f
miracline2 <112369890+miracline2@users.noreply.github.com>
2022-08-30 18:12:45 +0530

    Create version.js

---
+++ version.js
@@ -0,0 +1,27 @@
+Version=[
+    v1={
+    Name:"hello",
+    releaseDate:"22-03-2022",
+    bug:['compability crash bugs'],
+    features:["biometric sensor"],
+    Ver:["v1"],
+    author:["simran","jyothika"]
+},
+V2={
+    Name:"hi",
+    releaseDate:"28-04-2022",
+    bug:['Network issue'],
+    features:["advance graphics"],
+    ver:["v2"],
+    author:["nithya","mirac"]
+},
+v3={
+    Name:"hey",
+    releaseDate:"18-05-2022",
+    bug:['glitch'],
+    features:["sound quality"],
+    ver:["v3"],
+    author:["nithya","simran","joe S"]
+}
+]
+console.table(Version)
